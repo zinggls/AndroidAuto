dc5dc89bee10f6d8923027731f0ed951450bd6e9
Rossi Kwak <rossi.kwak@gmail.com>
2020-11-19 17:39:53 +0900

    Send received buffer using Zing_DataWrite. Note that Zing_DataWrite function sends to GpifDataOut(Dma.DataOut_.Channel_) channel

--- AutoUsbToZing.c
+++ AutoUsbToZing.c
@@ -40,7 +40,11 @@ AutoUsbToZingThread(
 	CyU3PDebugPrint(4,"[A-Z] AutoDataIn.size=%d\n",glChHandleAutoDataIn.size);
 	while(1){
 		if((Status=Zing_Transfer_Recv(&glChHandleAutoDataIn,buf,&rt_len,CYU3P_WAIT_FOREVER))==CY_U3P_SUCCESS) {
-			CyU3PDebugPrint(4,"A");
+			if((Status=Zing_DataWrite(buf,rt_len))==CY_U3P_SUCCESS) {
+				CyU3PDebugPrint(4,"A");
+			}else{
+				CyU3PDebugPrint (4, "[A-Z] Zing_DataWrite error(0x%x)\n",Status);
+			}
 		}else{
 			CyU3PDebugPrint (4, "[A-Z] Zing_Transfer_Recv error(0x%x)\n",Status);
 		}

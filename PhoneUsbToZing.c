5eeaa33cc4742cf4d6837604b9481a087cca1e63
Rossi Kwak <rossi.kwak@gmail.com>
2020-11-23 17:34:37 +0900

    Implement while loop Ref. Cypress usbhost example

--- PhoneUsbToZing.c
+++ PhoneUsbToZing.c
@@ -3,6 +3,8 @@
 #include "cyu3system.h"
 #include "cyu3error.h"
 #include "Zing.h"
+#include "phonedrv.h"
+#include "cyu3usbhost.h"
 
 CyU3PThread PhoneUsbToZingThreadHandle;
 extern CyU3PDmaChannel glChHandlePhoneDataIn;
@@ -33,14 +35,40 @@ void
 PhoneUsbToZingThread(
 		uint32_t Value)
 {
-	uint32_t rt_len;
-	uint8_t *buf = (uint8_t *)CyU3PDmaBufferAlloc (glChHandlePhoneDataIn.size);
 	CyU3PReturnStatus_t Status;
+	CyU3PDmaBuffer_t Buf;
+	CyU3PUsbHostEpStatus_t epStatus;
 
 	CyU3PDebugPrint(4,"[P-Z] Phone USB to Zing thread starts\n");
 	CyU3PDebugPrint(4,"[P-Z] PhoneDataIn.size=%d\n",glChHandlePhoneDataIn.size);
+
+	Buf.buffer = (uint8_t *)CyU3PDmaBufferAlloc (glChHandlePhoneDataIn.size);
+	Buf.count = 0;
+	Buf.size = glChHandlePhoneDataIn.size;
+	Buf.status = 0;
 	while(1){
-        CyU3PThreadSleep (1000);
-        /* Do nothing */
+		Status = CyU3PDmaChannelSetupRecvBuffer (&glChHandlePhoneDataIn, &Buf);
+		if(Status!=CY_U3P_SUCCESS) {
+			CyU3PDebugPrint (4, "PhoneUsbToZingThread, CyU3PUsbHostEpSetXfer error(%d)\n", Status);
+			continue;
+		}
+
+        Status = CyU3PUsbHostEpSetXfer (Phone.inEp, 0, glChHandlePhoneDataIn.size);
+        if(Status!=CY_U3P_SUCCESS) {
+			CyU3PDebugPrint (4, "PhoneUsbToZingThread, CyU3PUsbHostEpSetXfer error(%d)\n", Status);
+			continue;
+        }
+
+        Status = CyU3PUsbHostEpWaitForCompletion (Phone.inEp, &epStatus, CYU3P_WAIT_FOREVER);
+        if(Status!=CY_U3P_SUCCESS) {
+			CyU3PDebugPrint (4, "PhoneUsbToZingThread, CyU3PUsbHostEpWaitForCompletion error(%d)\n", Status);
+			continue;
+        }
+
+        Status = CyU3PDmaChannelWaitForCompletion (&glChHandlePhoneDataIn,CYU3P_NO_WAIT);
+        if(Status!=CY_U3P_SUCCESS) {
+			CyU3PDebugPrint (4, "PhoneUsbToZingThread, CyU3PDmaChannelWaitForCompletion error(%d)\n", Status);
+        }
+        CyU3PDebugPrint (4, "P");
 	}
 }

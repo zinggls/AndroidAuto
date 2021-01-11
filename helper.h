70adecb3e230b45b9d8f6ab1eff0435f890bbf09
Rossi Kwak <rossi.kwak@gmail.com>
2021-01-11 10:24:55 +0900

    extract SendMessage into independent module

---
+++ helper.h
@@ -0,0 +1,8 @@
+#ifndef __HELPER_H__
+#define __HELPER_H__
+
+void
+SendMessage (
+	const char *msg);
+
+#endif
--- host.c
+++ host.c
@@ -88,6 +88,7 @@
 #include "dma.h"
 #include "ZingHw.h"
 #include "ControlCh.h"
+#include "helper.h"
 
 CyU3PThread applnThread;                        /* Application thread structure */
 CyU3PEvent  applnEvent;                         /* Event group used to signal the thread. */
@@ -236,41 +237,6 @@ CyFxSendSetupRqt (
             CY_FX_HOST_EP0_WAIT_TIMEOUT);
 
     return status;
-}
-
-void
-SendMessage (const char *msg)
-{
-	CyU3PReturnStatus_t status;
-	PacketFormat *pf;
-	if((pf=(PacketFormat*)CyU3PDmaBufferAlloc(520))==0){
-		CyU3PDebugPrint(4,"[Z-A] %s, PacketFormat CyU3PDmaBufferAlloc error\r\n",msg);
-		return;
-	}
-
-	pf->size = strlen(msg);
-	CyU3PMemCopy (pf->data,(uint8_t*)msg,pf->size);
-	if ((status = Zing_DataWrite((uint8_t*)pf, pf->size+sizeof(uint32_t))) == CY_U3P_SUCCESS) {
-#ifdef DEBUG
-		CyU3PDebugPrint(4,"[Phone] %s %d bytes sent\n",msg,pf->size);
-#endif
-	}else{
-		CyU3PDebugPrint(4,"[Phone] %s sent failed error: %d\n",msg,status);
-		CyU3PThreadSleep (10);
-		CyU3PDeviceReset(CyFalse);
-	}
-	CyU3PDmaBufferFree(pf);
-
-	/* Experiment, send message via control channel */
-	Zing_Header2(Dma.ControlOut_.Buffer_,1,0,0,0,0,1,0,0,strlen(msg));
-	memcpy(Dma.ControlOut_.Buffer_+ZING_HDR_SIZE, (uint8_t*)msg, strlen(msg));
-	if ((status = Zing_Transfer_Send(&Dma.ControlOut_.Channel_,Dma.ControlOut_.Buffer_,strlen(msg)+ZING_HDR_SIZE)) == CY_U3P_SUCCESS) {
-#ifdef DEBUG
-		CyU3PDebugPrint(4,"[Phone] %s %d bytes sent\n",msg,strlen(msg));
-#endif
-	}else{
-		CyU3PDebugPrint(4,"[Phone] %s sent failed error: %d\n",msg,status);
-	}
 }
 
 void
